9fd7c96de90769c2ff43da68cfff3b3cd0747a78
Kendall Jordan <kendallajordan@gmail.com>
2022-01-26 19:07:12 -0800

    Updated CartProvider: Added cartReducer to hold context state

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,39 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (prevState, action) => {
+    if (action.type === "ADD"){
+        const updatedItems = prevState.items.concat(action.item);
+        const updatedTotalAmount = prevState.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+      dispatchCartAction({type: "ADD", item: item});
+  };
+
+  const removeItemFromCartHandler = (id) => {
+      dispatchCartAction({type: "REMOVE", id: id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
