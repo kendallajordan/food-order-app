7cd5606352ef9c390d2dce40a5ef3981b6786d0e
Kendall Jordan <kendallajordan@gmail.com>
2022-01-26 22:11:29 -0800

    Updated Cart: Add and remove buttons for cart items now functional.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -34,7 +34,33 @@ const cartReducer = (prevState, action) => {
 
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = prevState.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = prevState.items[existingCartItemIndex];
+    const updatedTotalAmount = prevState.totalAmount - existingCartItem.price;
+    let updatedItems;
+
+    // Remove item from cart if amount is 1. Else, keep item and decrease amount by 1.
+    if (existingCartItem.amount === 1) {
+      updatedItems = prevState.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...prevState.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
     };
   }
 
